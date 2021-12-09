3764c0d75ee52d0bbb22b04d2e7b0508ff1fa67f
Anand <anandkiot@gmail.com>
2021-12-09 12:04:38 +0530

    inital commit

--- apps/profile/src/app/app.component.spec.ts
+++ apps/profile/src/app/app.component.spec.ts
@@ -16,10 +16,10 @@ describe("AppComponent", () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'mdmf-shell'`, () => {
+  it(`should have as title 'mdmf-profile'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual("mdmf-shell");
+    expect(app.title).toEqual("mdmf-profile");
   });
 
   it("should render title", () => {
@@ -27,7 +27,7 @@ describe("AppComponent", () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
     expect(compiled.querySelector(".content span").textContent).toContain(
-      "mdmf-shell app is running!"
+      "mdmf-profile app is running!"
     );
   });
 });
--- apps/shellapp/src/app/app.component.ts
+++ apps/shellapp/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { MicrofrontendService } from './microfrontends/microfrontend.service';
 
 @Component({
-  selector: 'myorg-root',
+  selector: 'myorg-app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'shellapp';
+  constructor(public mfService: MicrofrontendService) {}
 }
